98aacd59665feff5dc8840dcd54d5f1bf163e1af
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-01 18:16:12 +0530

    made using template

--- script_by_using_template_html.js
+++ script_by_using_template_html.js
@@ -3,50 +3,42 @@ const searchMovieForm = document.getElementById("searchForm");
 searchMovieForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const search = document.getElementById("searchMovie");
-  removeAllChildNodes();
   fetchMovieData(search.value);
   search.value = "";
 });
 
 async function fetchMovieData(search) {
-  const parentCard = document.querySelector(".cards-parent");
+  const cardsParent = document.querySelector(".cards-parent");
   try {
     const response = await fetch(
       `https://www.omdbapi.com/?s=${search}&apikey=ee62d61b`
     );
     const movieData = await response.json();
-    const moviesAllCards = movieData.Search.reduce(
-      (acc, curr) => acc + makeCard(curr),
-      ""
-    );
-
-    parentCard.insertAdjacentHTML("afterbegin", moviesAllCards);
+    bindMovieData(movieData.Search);
   } catch (error) {
-    parentCard.insertAdjacentHTML("afterbegin", "<h1>Oops...Not found!</h1>");
+    cardsParent.innerHTML = "<h1>Oops...Not found!</h1>";
   }
 }
 
-function makeCard(card) {
-  const cardHtml =
-    card.Poster !== "N/A"
-      ? `
-    <div class="card">
-            <img src=${card.Poster}
-                alt="not available" class="card-image">
-                <div class="card-content">
-            <h2>Title : ${card.Title}</h2>
-            <h3>Year : ${card.Year}</h3>
-            <h4 class="movie-type">Type : <span>${card.Type.toUpperCase()}</span></h4>
-            </div>
-        </div>
-    `
-      : "<div></div>";
-  return cardHtml;
+function bindMovieData(movies) {
+  const parentCard = document.querySelector(".cards-parent");
+  const movieCardTemplate = document.getElementById("movie-card-template");
+  parentCard.innerHTML = "";
+  movies.map((movie) => {
+    if (movie.Poster === "N/A") return;
+    const cardClone = movieCardTemplate.content.cloneNode(true);
+    makeCard(cardClone, movie);
+    parentCard.appendChild(cardClone);
+  });
 }
+function makeCard(cardClone, movie) {
+  const cardImg = cardClone.querySelector(".card-image");
+  const cardTitle = cardClone.querySelector("#title span");
+  const cardYear = cardClone.querySelector("#year span");
+  const cardType = cardClone.querySelector(".movie-type span");
 
-function removeAllChildNodes() {
-  const parent = document.querySelector(".cards-parent");
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
+  cardImg.src = movie.Poster;
+  cardTitle.innerHTML = movie.Title;
+  cardYear.innerHTML = movie.Year;
+  cardType.innerHTML = movie.Type.toUpperCase();
 }
